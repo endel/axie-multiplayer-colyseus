063e1972b41ca1a659dcdb696bfe9df781ea0ad4
Pedro Fontoura <me@pedrofontoura.io>
2023-10-23 10:03:48 -0300

    fix: update use-colyseus to fix collection rendering issue

--- client/src/components/player/players.tsx
+++ client/src/components/player/players.tsx
@@ -5,30 +5,22 @@ import RemotePlayer from "./remote-player";
 import { randomPosition } from "../../utils";
 import { useColyseusRoom, useColyseusState } from "../../net";
 
-import type { Player } from "../../../../server/src/rooms/schema/MyRoomState";
-import type { MapSchema } from "@colyseus/schema";
-
 export default function Players() {
   const room = useColyseusRoom();
 
-  //
-  // TODO: adding/removing players doesn't seem to be triggering here
-  //
-  const players: MapSchema<Player> =
-    // @ts-ignore
-    useColyseusState((state) => state.players);
+  const players = useColyseusState((state) => state.players);
 
   const initialPosition = useMemo(() => randomPosition(), []);
 
   return (
     <group name="players">
-      {Array.from(players.keys()).map((sessionId) => (
-        (sessionId === room.sessionId) ? (
+      {Array.from(players.keys()).map((sessionId) =>
+        sessionId === room.sessionId ? (
           <LocalPlayer key={sessionId} position={initialPosition} />
         ) : (
           <RemotePlayer key={sessionId} player={players.get(sessionId)} />
         )
-      ))}
+      )}
     </group>
   );
 }
